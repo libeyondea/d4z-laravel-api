d1c47bd67e869f011418475391d4f26fbdb7da27
nhtanonymous <57558120+nhtanonymous@users.noreply.github.com>
2020-09-17 22:24:18 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -97,9 +97,10 @@ class AuthController extends Controller
                 'success' => true,
                 'data' => [
                     'id' => $user->id,
+                    'first_name' => $user->first_name,
+                    'last_name' => $user->last_name,
                     'user_name' => $user->user_name,
-                    'email' => $request->email,
-                    'token' => $token,
+                    'email' => $request->email
                 ]
             ];
         } else {
