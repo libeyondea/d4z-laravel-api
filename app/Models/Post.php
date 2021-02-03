4cc3d6f42fc029ed72f3107822af6b920256d0a5
nhtanonymous <57558120+nhtanonymous@users.noreply.github.com>
2021-02-03 09:57:47 +0700

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //use \App\Http\Traits\UsesUuid;
+    use \App\Http\Traits\UsesUuid;
 
     protected $table = 'post';
     protected $primaryKey = 'id';
